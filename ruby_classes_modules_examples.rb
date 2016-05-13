686146e3671fbdce843e9102456639b9bc972b12
mghadley <matthew.g.hadley@gmail.com>
2016-05-13 10:48:06 -0600

    first commit

--- ruby_classes_modules_examples.rb
+++ ruby_classes_modules_examples.rb
@@ -1,10 +1,15 @@
+require 'pry'
+require 'babbler'
+
 puts 'YAY Friday!'
 
 def keep_coding
   puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
   input = gets.strip.downcase
+
   if input == 'yes'
     puts 'Good answer!'
+    puts Babbler.babble
   else
     puts 'You serious, bro? Do you even code?'
     keep_coding
